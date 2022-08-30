38de6aa8b6593375557c7dec926f27781d6e3ac0
Kenny <djklu31@gmail.com>
2022-08-30 10:28:27 -0700

    Build for prod

--- src/App.jsx
+++ src/App.jsx
@@ -89,6 +89,7 @@ export default function App(props) {
                             );
                             props.openSettings();
                             clearTimer();
+                            return;
                         }
                     }
                 } else {
